07e98056bc55de3bdf0e93f1958d9a4183795b5f
SUFIYANJT <93928905+SUFIYANJT@users.noreply.github.com>
2024-07-30 15:46:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-document.addEventListener('scroll', () => {
-    const sRectangles = document.querySelectorAll('.srectangle1');
-    sRectangles.forEach(sRectangle => {
-        sRectangle.style.animation = 'none';
-        sRectangle.offsetHeight; /* trigger reflow */
-        sRectangle.style.animation = 'slideInRight 1s forwards';
-    });
-});
+
