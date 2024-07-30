7ea33cc0d31e1d314d151c83f3157b62526ee496
joelmathewthomas <0qx76r@proton.me>
2024-07-30 16:40:13 +0530

    vi

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+
 function vibrateOnClick() {
+    var button1=document.getElementById("button1");
     if (navigator.vibrate) {
         navigator.vibrate(10000); // Vibrate for 100 milliseconds
+        button1.innerHTML="viberated"
+    }
+    else{
+        button1.innerHTML="not viberated"
     }
 }
