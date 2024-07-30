f577cea089b3cb6da3e11e082a936104e5d51479
joelmathewthomas <0qx76r@proton.me>
2024-07-30 16:55:50 +0530

    bi

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-
+// index.js
 function vibrateOnClick() {
-    var button1=document.getElementById("button1");
     if (navigator.vibrate) {
-        navigator.vibrate(10000); // Vibrate for 100 milliseconds
-        button1.innerHTML="viberated"
-    }
-    else{
-        button1.innerHTML="not viberated"
+        navigator.vibrate(100); // Vibrate for 100 milliseconds
+    } else {
+        console.log("Vibration API is not supported on this device.");
     }
 }
